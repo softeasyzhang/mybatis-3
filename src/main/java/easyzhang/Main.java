d59d5cbe465d87dae1ca8533053f79e7f86b49c7
Easyzhang <softeasyzhang@gmail.com>
2021-05-30 20:46:13 +0800

    mybatis-proxy

--- src/main/java/easyzhang/Main.java
+++ src/main/java/easyzhang/Main.java
@@ -1,12 +1,16 @@
 package easyzhang;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.logging.log4j.Log4jImpl;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author EasyZhang
@@ -20,10 +24,15 @@ public class Main {
      * @param args
      */
     public static void main(String[] args){
+
+
         String resource = "easyzhang/mybatis-config.xml";
         try(InputStream inputStream = Resources.getResourceAsStream(resource)){
-            //加载配置文件
+            System.setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
+
+          //加载配置文件
             SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            Configuration configuration = sqlSessionFactory.getConfiguration();
             System.out.println("初始化完毕,开始加载....");
 
             //DefaultSqlSession <-这个才是实现类
@@ -33,7 +42,9 @@ public class Main {
             criteria.andCityIdEqualTo(1);
             CityMapper cityMapper = session.getMapper(CityMapper.class);
             List<City> cities = cityMapper.selectByExample(cityExample);
-            System.out.println(cities.get(0).getCityName());
+            //这里应该会走缓存
+            List<City> cities2 = cityMapper.selectByExample(cityExample);
+            System.out.println(cities2.get(0).getCityName());
             session.close();
 
         }catch (Exception e) {
